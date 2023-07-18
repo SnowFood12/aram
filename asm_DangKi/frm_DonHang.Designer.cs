eee78b99a55028049fff37752feb5339f7298111
LeSyKien <lesykien030@gmail.com>
2023-07-18 13:12:12 +0700

    sửa lại giao diện tạo hoá đơn

--- asm_DangKi/frm_DonHang.Designer.cs
+++ asm_DangKi/frm_DonHang.Designer.cs
@@ -98,7 +98,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 38);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1225, 704);
+            this.tabPage2.Size = new System.Drawing.Size(1372, 805);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Lịch sử giao dịch";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -107,7 +107,7 @@
             // 
             this.tabPage3.Location = new System.Drawing.Point(4, 38);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1225, 704);
+            this.tabPage3.Size = new System.Drawing.Size(1372, 805);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Doanh thu";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -115,7 +115,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 42);
+            this.label1.Location = new System.Drawing.Point(47, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(146, 29);
             this.label1.TabIndex = 0;
@@ -124,7 +124,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(59, 256);
+            this.label2.Location = new System.Drawing.Point(667, 391);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(118, 29);
             this.label2.TabIndex = 0;
@@ -132,32 +132,32 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(304, 42);
+            this.textBox1.Location = new System.Drawing.Point(293, 58);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(235, 36);
+            this.textBox1.Size = new System.Drawing.Size(264, 36);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.textBox2.Location = new System.Drawing.Point(304, 253);
+            this.textBox2.Location = new System.Drawing.Point(913, 388);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(235, 36);
+            this.textBox2.Size = new System.Drawing.Size(265, 36);
             this.textBox2.TabIndex = 1;
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(304, 140);
+            this.dateTimePicker1.Location = new System.Drawing.Point(293, 156);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(235, 36);
+            this.dateTimePicker1.Size = new System.Drawing.Size(264, 36);
             this.dateTimePicker1.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(59, 147);
+            this.label3.Location = new System.Drawing.Point(47, 163);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(212, 29);
             this.label3.TabIndex = 0;
@@ -166,7 +166,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(656, 42);
+            this.label4.Location = new System.Drawing.Point(667, 58);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(137, 29);
             this.label4.TabIndex = 0;
@@ -175,7 +175,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(656, 147);
+            this.label5.Location = new System.Drawing.Point(667, 163);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(219, 29);
             this.label5.TabIndex = 0;
@@ -184,16 +184,16 @@
             // textBox4
             // 
             this.textBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.textBox4.Location = new System.Drawing.Point(902, 138);
+            this.textBox4.Location = new System.Drawing.Point(913, 154);
             this.textBox4.Name = "textBox4";
             this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(235, 36);
+            this.textBox4.Size = new System.Drawing.Size(265, 36);
             this.textBox4.TabIndex = 1;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(656, 255);
+            this.label6.Location = new System.Drawing.Point(667, 271);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(175, 29);
             this.label6.TabIndex = 0;
@@ -202,7 +202,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(656, 361);
+            this.label7.Location = new System.Drawing.Point(47, 277);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(121, 29);
             this.label7.TabIndex = 0;
@@ -211,23 +211,23 @@
             // textBox6
             // 
             this.textBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.textBox6.Location = new System.Drawing.Point(902, 352);
+            this.textBox6.Location = new System.Drawing.Point(293, 268);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(235, 36);
+            this.textBox6.Size = new System.Drawing.Size(264, 36);
             this.textBox6.TabIndex = 1;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(902, 252);
+            this.comboBox1.Location = new System.Drawing.Point(913, 268);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(235, 37);
+            this.comboBox1.Size = new System.Drawing.Size(265, 37);
             this.comboBox1.TabIndex = 3;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(656, 450);
+            this.label8.Location = new System.Drawing.Point(47, 397);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(59, 29);
             this.label8.TabIndex = 0;
@@ -236,25 +236,25 @@
             // textBox5
             // 
             this.textBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.textBox5.Location = new System.Drawing.Point(902, 441);
+            this.textBox5.Location = new System.Drawing.Point(293, 388);
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(235, 36);
+            this.textBox5.Size = new System.Drawing.Size(264, 36);
             this.textBox5.TabIndex = 1;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(902, 39);
+            this.comboBox2.Location = new System.Drawing.Point(913, 55);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(235, 37);
+            this.comboBox2.Size = new System.Drawing.Size(265, 37);
             this.comboBox2.TabIndex = 3;
             // 
             // tabPage4
             // 
             this.tabPage4.Location = new System.Drawing.Point(4, 38);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1225, 704);
+            this.tabPage4.Size = new System.Drawing.Size(1372, 805);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Thanh toán";
             this.tabPage4.UseVisualStyleBackColor = true;
